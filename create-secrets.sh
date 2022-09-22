3778c9708e48f934c66a6c9d0ddecc584973f35c
Brandon White <bjwhite@fnal.gov>
2022-09-22 09:23:39 -0700

    Update create-secrets.sh with appropriate options for new service certificate.

--- create-secrets.sh
+++ create-secrets.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+# FTS3 Delegation Secrets
+kubectl create secret generic usdf-rucio-fts-cert \
+        --from-file=usercert.pem=${PWD}/secret/hostcert.pem \
+
+kubectl create secret generic usdf-rucio-fts-key \
+	--from-file=new_userkey.pem=${PWD}/secret/hostkey.pem
+
 # Daemon Secrets
 kubectl create secret generic usdf-rucio-x509up \
-        --from-file=hostcert.pem=${PWD}/secret/rubin-rucio.slac.stanford.edu-cert.pem \
-        --from-file=hostkey.pem=${PWD}/secret/rubin-rucio.slac.stanford.edu-key.pem
+        --from-file=hostcert.pem=${PWD}/secret/hostcert.pem \
+        --from-file=hostkey.pem=${PWD}/secret/hostkey.pem
 
 kubectl create secret generic usdf-rucio-ca-bundle \
-        --from-file=ca.pem.pem=${PWD}/secret/ca_bundle.pem
+        --from-file=ca.pem.pem=${PWD}/secret/ca.pem
 
 # Ingress Secrets
 kubectl create secret tls rucio-server.tls-secret \
-        --cert=${PWD}/secret/rubin-rucio.slac.stanford.edu-cert.pem \
-        --key=${PWD}/secret/rubin-rucio.slac.stanford.edu-key.pem
+        --cert=${PWD}/secret/hostcert.pem \
+        --key=${PWD}/secret/hostkey.pem
