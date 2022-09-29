81d7097ceac9df57e38be7c37419d5d4343af5a3
Brandon White <bjwhite@fnal.gov>
2022-09-29 08:20:41 -0700

    Update secrets.

--- create-secrets.sh
+++ create-secrets.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # FTS3 Delegation Secrets
-kubectl create secret generic usdf-rucio-fts-cert \
+kubectl -n rucio create secret generic usdf-rucio-fts-cert \
         --from-file=usercert.pem=${PWD}/secret/hostcert.pem \
 
-kubectl create secret generic usdf-rucio-fts-key \
+kubectl -n rucio create secret generic usdf-rucio-fts-key \
 	--from-file=new_userkey.pem=${PWD}/secret/hostkey.pem
 
 # Daemon Secrets
-kubectl create secret generic usdf-rucio-x509up \
+kubectl -n rucio create secret generic usdf-rucio-x509up \
         --from-file=hostcert.pem=${PWD}/secret/hostcert.pem \
         --from-file=hostkey.pem=${PWD}/secret/hostkey.pem
 
-kubectl create secret generic usdf-rucio-ca-bundle \
-        --from-file=ca.pem.pem=${PWD}/secret/ca.pem
+kubectl -n rucio create secret generic usdf-rucio-ca-bundle \
+        --from-file=ca.pem=${PWD}/secret/ca.pem
 
 # Ingress Secrets
-kubectl create secret tls rucio-server.tls-secret \
+kubectl -n rucio create secret tls rucio-server.tls-secret \
         --cert=${PWD}/secret/hostcert.pem \
         --key=${PWD}/secret/hostkey.pem
 
@@ -24,6 +24,16 @@ kubectl create secret tls rucio-server.tls-secret \
 mkdir /tmp/reaper-certs
 cp /etc/grid-security/certificates/*.0 /tmp/reaper-certs/
 cp /etc/grid-security/certificates/*.signing_policy /tmp/reaper-certs/
-kubectl create secret generic usdf-rucio-ca-bundle-reaper \
+kubectl -n rucio create secret generic usdf-rucio-ca-bundle-reaper \
         --from-file=/tmp/reaper-certs
 rm -r /tmp/reaper-certs
+
+# WebUI
+kubectl -n rucio create secret generic usdf-hostcert \
+        --from-file=hostcert.pem=${PWD}/secret/hostcert.pem
+
+kubectl -n rucio create secret generic usdf-hostkey \
+        --from-file=hostkey.pem=${PWD}/secret/hostkey.pem
+
+kubectl -n rucio create secret generic usdf-cafile \
+        --from-file=ca.pem=${PWD}/secret/ca.pem
