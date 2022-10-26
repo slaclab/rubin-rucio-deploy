4507ac98dc49ac1dec602d040195674ddb4d0690
Brandon White <bjwhite@fnal.gov>
2022-10-26 09:54:49 -0700

    Add Prod deployment config/method.

--- secrets2vault.sh
+++ secrets2vault.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-cd ./rubin-rucio-dev-secrets	
-vault kv put secret/rubin/usdf-rucio-dev/rucio usdf-server-hostcert=@hostcert.pem usdf-server-hostkey=@hostkey.pem usdf-server-cafile=@ca.pem
+if [ $# -eq 0 ]; then
+    echo "Please specify the deployment to upload secrets into Vault for (dev|prod)."
+    exit -1
+fi
+
+if [ $1 == "dev" ]; then
+    cd ./secret
+    vault kv put secret/rubin/usdf-rucio-dev/rucio usdf-server-hostcert=@dev/hostcert.pem usdf-server-hostkey=@dev/hostkey.pem usdf-server-cafile=@ca.pem
+elif [ $1 == "prod" ]; then
+    cd ./secret
+    vault kv put secret/rubin/usdf-rucio/rucio usdf-server-hostcert=@prod/hostcert.pem usdf-server-hostkey=@prod/hostkey.pem usdf-server-cafile=@ca.pem
+else
+    echo "Invalid deployment specification. (dev|prod)"
+    exit -2
+fi
