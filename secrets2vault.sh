1859d4a6b7e859bebeef5a13e50f3899d19dcede
yuyiguo <yuyi@fnal.gov>
2022-10-27 16:21:36 -0500

    uodated for prod DB deployment and DB creds to vault.

--- secrets2vault.sh
+++ secrets2vault.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd ./rubin-rucio-dev-secrets	
 vault kv put secret/rubin/usdf-rucio-dev/rucio usdf-server-hostcert=@hostcert.pem usdf-server-hostkey=@hostkey.pem usdf-server-cafile=@ca.pem
+vault kv put secret/rubin/usdf-rucio/postgres username=rubin password=anything-you-want-to
+
