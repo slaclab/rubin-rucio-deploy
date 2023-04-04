a05ee4f776436f3949693d817971be136bbecb20
Brandon White <bjwhite@fnal.gov>
2023-04-04 09:17:17 -0700

    Update LSST schema for DID names.

--- overlays/dev/rucio/etc/lsst.py
+++ overlays/dev/rucio/etc/lsst.py
@@ -57,7 +57,7 @@ NAME_LENGTH = 500
 
 NAME = {"description": "Data Identifier name",
         "type": "string",
-        "pattern": r"^/[A-Za-z0-9._+/-]{1,%s}$" % NAME_LENGTH}
+        "pattern": r"^[A-Za-z0-9._+/-]{1,%s}$" % NAME_LENGTH}
 
 R_NAME = {"description": "Data Identifier name",
           "type": "string",
