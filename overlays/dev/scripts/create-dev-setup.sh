3bd91234eb478067b64551af2446f66d8496a743
Brandon White <bjwhite@fnal.gov>
2024-02-13 15:01:15 -0700

    Silly fixes.

--- overlays/dev/scripts/create-dev-setup.sh
+++ overlays/dev/scripts/create-dev-setup.sh
@@ -7,38 +7,38 @@ FTS_SERVERS=https://lcgfts3.gridpp.rl.ac.uk:8446
 rucio-admin -a root account add bjwhite
 rucio-admin -a root identity add --account bjwhite --type X509 --email 'bjwhite@fnal.gov' --id '/DC=org/DC=cilogon/C=US/O=Fermi National Accelerator Laboratory/OU=People/CN=Brandon White/CN=UID:bjwhite'
 rucio-admin -a root account add-attribute --key admin --value True bjwhite
-rucio-admin -a root scope add user.bjwhite
+rucio-admin -a root scope add --account bjwhite --scope user.bjwhite
 
 # Yuyi Guo
 rucio-admin -a root account add yuyi 
 rucio-admin -a root identity add --account yuyi --type X509 --email 'yuyi@fnal.gov' --id '/DC=org/DC=cilogon/C=US/O=Fermi National Accelerator Laboratory/OU=People/CN=Yuyi Guo/CN=UID:yuyi'
 rucio-admin -a root account add-attribute --key admin --value True yuyi
-rucio-admin -a root scope add user.yuyi
+rucio-admin -a root scope add --account yuyi --scope user.yuyi
 
 # Greg Daues
 rucio-admin -a root account add daues
 rucio-admin -a root identity add --account daues --type X509 --email 'daues@illinois.edu' --id '/DC=org/DC=cilogon/C=US/O=Fermi National Accelerator Laboratory/OU=People/CN=Gregory Daues/CN=UID:daues'
 rucio-admin -a root account add-attribute --key admin --value True daues
-rucio-admin -a root scope add user.daues
+rucio-admin -a root scope add --account daues --scope user.daues
 
 # Fabio Hernandez
 user=fabioh
 rucio-admin -a root account add ${user}
 rucio-admin -a root identity add --account ${user} --type X509 --email 'fabio@in2p3.fr' --id '/O=GRID-FR/C=FR/O=CNRS/OU=CC-IN2P3/CN=Fabio Hernandez'
 rucio-admin -a root account add-attribute --key admin --value True ${user}
-rucio-admin -a root scope add user.fabioh
+rucio-admin -a root scope add --account fabioh --scope user.fabioh
 
 # Andy Hanushevsky
 rucio-admin -a root account add abh
 rucio-admin -a root identity add --account abh --type SSH --email 'abh.slac.stanford.edu' --id 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAwjVM9PV53oEOwnlQhmR5GuTIUAagF9vaEd6gJVfyNdTRh58acAk37UhRz1CfURVtyHlwoCVCUrX37QsifsUufredCs8g9PIIidLaVQagoFaj0+tgRQlfHU/aDEDbyOciKg4TLBQuOZX3ZcrEHamkKnEgamQBzt1MnU3CmdWQVDlC8N+/alN7mUivbRabLQ+m93WA48YoLJOPbF6mQ5uWd0nce6sv+/aWQsihH+zzoJaZUz4fs82CHbMuLsPZLu1N50UlSy/rudGJetX0MCGsSq4nnnGxRghB67r6a+NuwC1U3LrLXL6EQY5ux7EVsKz4rYj23m5UhSkXYasfLiTwbw== abh@io'
 rucio-admin -a root account add-attribute --key admin --value True abh
-rucio-admin -a root scope add user.abh
+rucio-admin -a root scope add --account abh --scope user.abh
 
 # Wei Yang
 rucio-admin -a root account add yangw
 rucio-admin -a root identity add --account yangw --type SSH --email 'yangw@slac.stanford.edu' --id 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDxdxD5mj4tciwdI5iRl6SW3L0BGHNNlX4G/JkUZ1/j3zWM9aTQ1U8/V9uaEFyzWCcak4PLzWKmz6QdQEC+X2tYtOuRdCcmx2lyBniAlunBdKXRetnUaQBRuuV0cYMR2V6qbPuBk816IsS1DwyDYZCe0C28/B3eJNjql6esm5jrqLBIwEdcf7O3kJ8SN9/XbHOoKWwGJHk0XHZlsGz3ID6XwY9Lvuu/bm5nY1Da+C/rrn2TosDcTKGlXJue4lJo3eX4xo/W/rWeGVGu3PVPYB6WQ+HGehaymvBeAHZMbfrJg/eadVbfSovkZ9La5/ZOplMV4gRi+HSP42q4Ns83GwkQWXP4Eo8Rw5LwZb2xrHC65htDSQH4dDNQI8GX63sF9QlXG7s59rRp8ZxmCGKDtWTuYDuAuQk7hV0nddIISTva0jdysNwcJz9D69AHFlfLfBQGTyayUxr4m05XoGx5Blm1dQE1B13EvAmMJHGvjtNrDQ+hpYmLBVDwJwsKZOo3/oOKiqU6SXYUaqo+5BysnCBwn++GBTrYFmX+AluMS1onWhuuI4d9DC+lHvvtdUzBVaO4MecU1tiWC1qQaBzltPkrA203IJsB6XIaPctcj/OgmlNvLVqX0HTVwsubbTxedyNUtlkYxHvaagui8jQOmjQPSmJFpHL3wb5VLP8ZCbFRxw== yangw@dhcp-swlan-public-81-142.slac.stanford.edu'
 rucio-admin -a root account add-attribute --key admin --value True yangw
-rucio-admin -a root scope add user.yangw
+rucio-admin -a root scope add --account yangw --scope user.yangw
 
 # Data Service
 rucio-admin -a root account add register_service
